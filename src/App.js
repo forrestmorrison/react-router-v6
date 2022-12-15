20c7f9243755223a46f4881b3b380bf36b862568
forrestmorrison <forrestmorrison@gmail.com>
2022-12-15 17:37:14 -0600

    Outlet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Link, Route, Routes } from 'react-router-dom';
-import { Home } from "./pages/Home"
-import { BookList } from "./pages/BookList"
-import { Book } from "./pages/Book"
-import { NewBook } from "./pages/NewBook"
-import { NotFound } from "./pages/NotFound"
+import { Home } from "./pages/Home";
+import { BookList } from "./pages/BookList";
+import { BookLayout } from "./BookLayout";
+import { Book } from "./pages/Book";
+import { NewBook } from "./pages/NewBook";
+import { NotFound } from "./pages/NotFound";
 import './App.css';
 
 function App() {
@@ -20,11 +21,13 @@ function App() {
       </ul>
     </nav>
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/books" element={<BookList />}/>
-      <Route path="/books/:id" element={<Book />}/>
-      <Route path="/books/new" element={<NewBook />}/>
-      <Route path="*" element={<NotFound />}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/books" element={<BookLayout />}>
+        <Route index element={<BookList />} />
+        <Route path=":id" element={<Book />} />
+        <Route path="new" element={<NewBook />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </>
   )
