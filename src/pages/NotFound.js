536f557b08d8a14bd28341b162545b0010a3ee55
forrestmorrison <forrestmorrison@gmail.com>
2022-12-15 19:31:38 -0600

    useNavigate Hook

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,12 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
 export function NotFound() {
+    const navigate = useNavigate()
+    useEffect(() => {
+        setTimeout(() => {
+            navigate(-1)
+        }, 1000)
+    }, [])
     return <h1>Not Found</h1>
 }
